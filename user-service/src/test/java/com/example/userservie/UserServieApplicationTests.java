6dbcd225efcb1617784070d106e87ec3ea393a68
kim jisu <jisu3316@naver.com>
2023-02-06 21:17:25 +0900

    init

---
+++ user-service/src/test/java/com/example/userservie/UserServieApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.userservie;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserServieApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
